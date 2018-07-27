111c10e6541ce149860619ebb66c1ef3c6e8b231
unknown <jweb180723@LAB11-WS12.info.local>
2018-07-26 21:57:31 -0300

    Aula 4 javascrpit e css

---
+++ Aula 2/wks-javaweb/GameOfPi/src/GameOfPI.java
@@ -0,0 +1,14 @@
+
+public class GameOfPI {
+
+	public static void main(String[] args) {
+	
+		for (int i = 0;i<=100;i++) {
+			if (i % 4 ==0  && i >= 4 )
+				System.out.println("PI");
+			else
+				System.out.println(i);
+		}
+	}
+	
+}
